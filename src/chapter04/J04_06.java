f076377fe2787150fbceef216c20d65f90fe80e4
ICTER Guru <icterguru@gmail.com>
2017-12-09 18:54:32 -0500

    This is my first test commit from home laptop

--- src/chapter04/J04_06.java
+++ src/chapter04/J04_06.java
@@ -9,9 +9,9 @@ public class J04_06 {
 	boolean result; 
 	 
 	
-	System.out.println("a  =  " + a);
-	System.out.println("b  =  " + b);
-	System.out.println("c  =  " + c);
+	System.out.println("a = " + a);
+	System.out.println("b = " + b);
+	System.out.println("c = " + c);
 
 	result = b > a;	
 	System.out.println("b > a =  " + result);
