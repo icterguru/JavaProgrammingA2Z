c1136d5a78ce06deca7bde786907bb5265588946
ICTER Guru <icterguru@gmail.com>
2017-12-10 17:59:35 -0500

    I have done another change

--- src/chapter03/J03_08.java
+++ src/chapter03/J03_08.java
@@ -5,7 +5,8 @@ public class J03_08 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	byte Value1 = 100;  
-	short Value2 = 10000; 
+	short Value2 = 1000; 
+	
 	int Mul;
 	Mul = Value1 * Value2; //Illegal Assignment
 	System.out.println("Value1: " +Value1);
