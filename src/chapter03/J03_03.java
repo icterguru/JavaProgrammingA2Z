315a18a3169dbee9e6c8c19074ea93e50be28998
Rahima Mokter <rahimamokter@gmail.com>
2017-12-10 18:15:09 -0500

    I have done one more changes just for test

--- src/chapter03/J03_03.java
+++ src/chapter03/J03_03.java
@@ -5,8 +5,8 @@ public class J03_03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	char Ch1 = 65, Ch2= 'A';
-	System.out.println("Ch1  is " +Ch1);
-	System.out.println("Ch2  is " + Ch2);
+	System.out.println("Ch1  is: " +Ch1);
+	System.out.println("Ch2 is " + Ch2);
 	
 	}
 
