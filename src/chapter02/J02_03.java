5d735e3a25c0045d429e1d424dcc3308f70a5aa4
Mokter Hossain <mhossa12@kennesaw.edu>
2018-11-15 11:33:05 -0500

    codes updated for J02_03.java

--- src/chapter02/J02_03.java
+++ src/chapter02/J02_03.java
@@ -5,18 +5,15 @@ package chapter02;
 public class J02_03{
 
 public static void main (String[] args )
- {	
- int i, c;
- String S;
- c = args.length;
- System.out.println("Number of arguments is : " +c);
+{	
+	int argCount = args.length;
+	System.out.println("Number of arguments is : " + argCount);
 
- for (i =0; i<c; i++)
-  {
-  S = args[i];
-  System.out.println("Arg["+i+"] is : " +S);
-  }
- 
- }
+	for (int i =0; i<args.length; i++)
+	{
+		System.out.println("Arg["+i+"] is : " + args[i]);
+	}
+
+}
 }
 
